0eb382f6cf9ace43f24e7d5c35953f654d6813bf
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2024-10-30 01:34:34 -0400

    Added the Generated Data Per NoBench

---
+++ bulk_insert.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+if [[ $(docker ps --filter "name=mongodb" --filter "status=running" --format "{{.Names}}") == "mongodb" ]]; then
+    echo "MongoDB is already running."
+else
+    echo "MongoDB is not running. Starting the MongoDB container..."
+    
+    # Try to start the MongoDB container
+    docker start mongodb
+    
+    # Check if it started successfully
+    if [[ $(docker ps --filter "name=mongodb" --filter "status=running" --format "{{.Names}}") == "mongodb" ]]; then
+        echo "MongoDB container started successfully."
+    else
+        echo "Failed to start the MongoDB container. Please check if the container exists."
+        exit 1
+    fi
+fi
+
+echo "Running NoSQLBench..."
+./nb5 ~/mongo_benchmark/activities/mongodb_insert_16.yaml
