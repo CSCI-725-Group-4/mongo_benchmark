20e2a82b89fd5b545abb09d1ba92ecd881deef73
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2024-11-25 00:44:33 -0500

    test new num

--- queries/queries.js
+++ queries/queries.js
@@ -13,16 +13,16 @@ const startTime = new Date();
 
 runTimedMethod(() => {
   const result = collection.find({}, ["str1", "num"]);
-  print("Result:", result);
+  // print("Result:", result);
 }, "Query 1")
 
 runTimedMethod(() => {
   const result = collection.find({}, ["nested_obj.str", "nested_obj.num"]);
-  print("Result:", result);
+  // print("Result:", result);
 }, "Query 2")
 
 runTimedMethod(() => {
-  const random_num = 90;
+  const random_num = 42;
   const sparse_0 = `sparse_${random_num}0`;
   const sparse_9 = `sparse_${random_num}9`;
   const result = collection.find(
