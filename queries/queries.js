7cbf2ebe259d3c4984481f84b3b2088eacd4fd0f
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2024-12-02 23:57:29 -0500

    Added Results

--- queries/queries.js
+++ queries/queries.js
@@ -3,6 +3,8 @@ const db = connect("localhost:27017/" + dbName);
 const collection = db.no_sql;
 const collection_size = 16000;
 
+const runTimes = []
+
 const runTimedMethod = (method, name, {skip, print}={skip: false, print: false}) => {
   if (skip) {
     console.log(`Skipping ${name}...`);
@@ -38,148 +40,179 @@ const calculateBounds = (size) => {
   return {lower_bound, upper_bound};
 };
 
-const totalElapsedTime = 0;
+runTimes.push(
+  runTimedMethod(() => {
+    return collection.find({}, ["str1", "num"]);
+  }, "Query 1")
+);
 
-totalElapsedTime += runTimedMethod(() => {
-  return collection.find({}, ["str1", "num"]);
-}, "Query 1")
+runTimes.push(
+  runTimedMethod(() => {
+    return collection.find({}, ["nested_obj.str", "nested_obj.num"]);
+  }, "Query 2")
+);
 
-totalElapsedTime += runTimedMethod(() => {
-  return collection.find({}, ["nested_obj.str", "nested_obj.num"]);
-}, "Query 2")
+runTimes.push(
+  runTimedMethod(() => {
+    const random_num = Math.floor(Math.random() * 100).toString().padStart(2, '0');
+    const sparse_0 = `sparse_${random_num}0`;
+    const sparse_9 = `sparse_${random_num}9`;
 
-totalElapsedTime += runTimedMethod(() => {
-  const random_num = Math.floor(Math.random() * 100).toString().padStart(2, '0');
-  const sparse_0 = `sparse_${random_num}0`;
-  const sparse_9 = `sparse_${random_num}9`;
+    const query = {
+      "$or": [{}, {}]
+    };
 
-  const query = {
-    "$or": [{}, {}]
-  };
-
-  query['$or'][0][sparse_0] = {
-    "$exists": true
-  }
-
-  query['$or'][1][sparse_9] = {
-    "$exists": true
-  }
-
-  return collection.find(
-    query,
-   [sparse_0, sparse_9]
-  );
-}, "Query 3");
-
-totalElapsedTime += runTimedMethod(() => {
-  const random_num_1 = Math.floor(Math.random() * 100).toString().padStart(2, '0');
-  const random_num_2 = Math.floor(Math.random() * 100).toString().padStart(2, '0');
-  while (random_num_2 == random_num_1) {
-    // Sanity check, make sure it's different
-    random_num_2 = Math.floor(Math.random() * 100).toString().padStart(2, '0');
-  }
-  const sparse_1 = `sparse_${random_num_1}0`;
-  const sparse_2 = `sparse_${random_num_2}0`;
-
-  const query = {
-    "$or": [{}, {}]
-  };
-
-  query['$or'][0][sparse_1] = {
-    "$exists": true
-  }
-
-  query['$or'][1][sparse_2] = {
-    "$exists": true
-  }
-
-  return collection.find(
-    query,
-   [sparse_1, sparse_2]
-  );
-}, "Query 4");
-
-totalElapsedTime += runTimedMethod(() => {
-  const rifleShotStr = 'RT2CJT9WQR5QI1VDGBJ9L3LR29ODAS6CW9EKKUEHTPK6TOP61G'
-
-  return collection.find({str1: rifleShotStr});
-}, "Query 5");
-
-totalElapsedTime += runTimedMethod(() => {
-  const {lower_bound, upper_bound} = calculateBounds(0.001);
-  return collection.find(
-    { 
-      "$and": [
-        { "num" : { "$gte" : lower_bound } }, 
-        { "num" : { "$lt" : upper_bound } }
-      ]
+    query['$or'][0][sparse_0] = {
+      "$exists": true
     }
-  );
-}, "Query 6");
 
-totalElapsedTime += runTimedMethod(() => {
-  const {lower_bound, upper_bound} = calculateBounds(0.001);
-  return collection.find(
-    { 
-      "$and": [
-        { "dyn1" : { "$gte" : lower_bound } }, 
-        { "dyn1" : { "$lt" : upper_bound } }
-      ]
+    query['$or'][1][sparse_9] = {
+      "$exists": true
     }
-  );
-}, "Query 7");
 
-totalElapsedTime += runTimedMethod(() => {
-  const query_string = 'He/pps took/vbd the/at suitcase/nn out/rp to/in the/at Jeep/nn-tl and/cc put/vbd it/ppo in/in the/at front/jj seat/nn ./.';
-  return collection.find(
-    { "nested_arr" : query_string }
-  );
-}, "Query 8");
+    return collection.find(
+      query,
+    [sparse_0, sparse_9]
+    );
+  }, "Query 3")
+);
 
-totalElapsedTime += runTimedMethod(() => {
-  const sparse000Str = 'VE0I3B3XGRP0AGEQ3NOO73TIRFWZZUZCZB7AQE1A6QVZZLWEPN'
-
-  return collection.find({sparse_000: sparse000Str});
-}, "Query 9", {print:true});
-
-totalElapsedTime += runTimedMethod(() => {
-  const {lower_bound, upper_bound} = calculateBounds(0.1);
-  return collection.aggregate([
-    {
-      $match: {
-        $and: [
-          { num: { $gte: lower_bound } },
-          { num: { $lt: upper_bound } }
-        ],
-        thousandth: {'$exists': true}
-      }
-    },
-    {
-      $group: {
-        _id: '$thousandth',
-        total: { $sum: 1 }
-      }
+runTimes.push(
+  runTimedMethod(() => {
+    const random_num_1 = Math.floor(Math.random() * 100).toString().padStart(2, '0');
+    const random_num_2 = Math.floor(Math.random() * 100).toString().padStart(2, '0');
+    while (random_num_2 == random_num_1) {
+      // Sanity check, make sure it's different
+      random_num_2 = Math.floor(Math.random() * 100).toString().padStart(2, '0');
     }
-  ]);
-}, "Query 10", {print:true});
+    const sparse_1 = `sparse_${random_num_1}0`;
+    const sparse_2 = `sparse_${random_num_2}0`;
 
-totalElapsedTime += runTimedMethod(() => {
-  const {lower_bound, upper_bound} = calculateBounds(0.001);
-  return collection.aggregate([
-    {
-      $match: {
-        num: { $gte: lower_bound, $lte: upper_bound }
-      }
-    },
-    {
-      $lookup: {
-        from: "no_sql",
-        localField: "nested_obj.str",
-        foreignField: "str1",
-        as: "matchedDocs"
-      }
+    const query = {
+      "$or": [{}, {}]
+    };
+
+    query['$or'][0][sparse_1] = {
+      "$exists": true
     }
-  ]);
-}, "Query 11", {print:true});
 
-console.log("Total Execution Time (ms):", totalElapsedTime);
+    query['$or'][1][sparse_2] = {
+      "$exists": true
+    }
+
+    return collection.find(
+      query,
+    [sparse_1, sparse_2]
+    );
+  }, "Query 4")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const rifleShotStr = 'RT2CJT9WQR5QI1VDGBJ9L3LR29ODAS6CW9EKKUEHTPK6TOP61G'
+
+    return collection.find({str1: rifleShotStr});
+  }, "Query 5")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const {lower_bound, upper_bound} = calculateBounds(0.001);
+    return collection.find(
+      { 
+        "$and": [
+          { "num" : { "$gte" : lower_bound } }, 
+          { "num" : { "$lt" : upper_bound } }
+        ]
+      }
+    );
+  }, "Query 6")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const {lower_bound, upper_bound} = calculateBounds(0.001);
+    return collection.find(
+      { 
+        "$and": [
+          { "dyn1" : { "$gte" : lower_bound } }, 
+          { "dyn1" : { "$lt" : upper_bound } }
+        ]
+      }
+    );
+  }, "Query 7")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const query_string = 'He/pps took/vbd the/at suitcase/nn out/rp to/in the/at Jeep/nn-tl and/cc put/vbd it/ppo in/in the/at front/jj seat/nn ./.';
+    return collection.find(
+      { "nested_arr" : query_string }
+    );
+  }, "Query 8")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const sparse000Str = 'VE0I3B3XGRP0AGEQ3NOO73TIRFWZZUZCZB7AQE1A6QVZZLWEPN'
+
+    return collection.find({sparse_000: sparse000Str});
+  }, "Query 9")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const {lower_bound, upper_bound} = calculateBounds(0.1);
+    return collection.aggregate([
+      {
+        $match: {
+          $and: [
+            { num: { $gte: lower_bound } },
+            { num: { $lt: upper_bound } }
+          ],
+          thousandth: {'$exists': true}
+        }
+      },
+      {
+        $group: {
+          _id: '$thousandth',
+          total: { $sum: 1 }
+        }
+      }
+    ]);
+  }, "Query 10")
+);
+
+runTimes.push(
+  runTimedMethod(() => {
+    const {lower_bound, upper_bound} = calculateBounds(0.001);
+    return collection.aggregate([
+      {
+        $match: {
+          num: { $gte: lower_bound, $lte: upper_bound }
+        }
+      },
+      {
+        $lookup: {
+          from: "no_sql",
+          localField: "nested_obj.str",
+          foreignField: "str1",
+          as: "matchedDocs"
+        }
+      }
+    ]);
+  }, "Query 11")
+);
+
+const totalElapsedTime = runTimes.reduce((partialSum, a) => partialSum + a, 0);
+
+console.log(
+  'Total Execution Time:', 
+  totalElapsedTime, 
+  `ms,`, 
+  (totalElapsedTime/1000).toFixed(2),
+  `s,`, 
+  (totalElapsedTime/60000).toFixed(2), 
+  `min.`);
+
+console.log(`${new Date().toISOString()},${runTimes.join(',')}`);
