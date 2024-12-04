4cfd057cafb835d02af0baed3552f4964651ff89
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2024-12-04 00:23:39 -0500

    Added Sinew Testing Results

--- queries/show_results.py
+++ queries/show_results.py
@@ -34,7 +34,7 @@ queries_1_thru_10 = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10'
 
 mongo_1_thru_10 = [2.33,   2.49,   1.1,    1.11,   0.99,   1.01,   1.02,   1.07,   1.09,   1]
 pgsql_1_thru_10 = [1.527,1.244,0.517,0.447,0.402,0.678,0.707,0.4,0.439,0.526]
-sinew_1_thru_10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+sinew_1_thru_10 = [0.842,0.949,0.91,0.943,0.819,0.0,0.0,1.014,0.0,0.749]
 
 query_11 = ['Q11']
 
@@ -42,9 +42,9 @@ mongo_11 = [18.08]
 pgsql_11= [13.83]
 sinew_11 = [0]
 
-# draw_plot(queries_1_thru_10, [(mongo_1_thru_10, "Mongo", -1), (pgsql_1_thru_10, "PGSQL", 0), (sinew_1_thru_10, "Sinew", 1)], padding=None)
-# draw_plot(query_11, [(mongo_11, "Mongo", -1), (pgsql_11, "PGSQL", 0), (sinew_11, "Sinew", 1)])
+draw_plot(queries_1_thru_10, [(mongo_1_thru_10, "Mongo", -1), (pgsql_1_thru_10, "PGSQL", 0), (sinew_1_thru_10, "Sinew", 1)], padding=None)
+draw_plot(query_11, [(mongo_11, "Mongo", -1), (pgsql_11, "PGSQL", 0), (sinew_11, "Sinew", 1)])
 
-draw_plot(list(itertools.chain(queries_1_thru_10, query_11)), [(list(itertools.chain(pgsql_1_thru_10, pgsql_11)), "PGSQL", 0)], padding=None, legend=False, bar_width=None)
-
+draw_plot(list(itertools.chain(queries_1_thru_10, query_11)), [(list(itertools.chain(sinew_1_thru_10, sinew_11)), "Sinew", 0)], padding=None, legend=False, bar_width=None)
+# draw_plot(list(itertools.chain(queries_1_thru_10, query_11)), [(list(itertools.chain(pgsql_1_thru_10, pgsql_11)), "PGSQL", 0)], padding=None, legend=False, bar_width=None)
 # draw_plot(list(itertools.chain(queries_1_thru_10, query_11)), [(list(itertools.chain(mongo_1_thru_10, mongo_11)), "Mongo", 0)], padding=None, legend=False, bar_width=None)
